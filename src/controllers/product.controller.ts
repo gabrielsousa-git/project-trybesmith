b15f675d5d1eb67efef18f2624c03e086701f43f
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-12-31 10:21:30 -0300

    :sparkles: 1 - Crie um endpoint para o cadastro de produtos

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import statusCodes from '../statusCodes';
+import ProductService from '../services/product.service';
+
+export default class ProductController {
+  constructor(private productService = new ProductService()) {}
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    res.status(statusCodes.CREATED).json(productCreated);
+  };
+}
