307934d1330873b4e3a01b594172967358950cfa
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-12-31 10:42:50 -0300

    :sparkles: feat: Req 2 - Crie um endpoint para a listagem de produtos

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -11,4 +11,9 @@ export default class ProductController {
     const productCreated = await this.productService.create(product);
     res.status(statusCodes.CREATED).json(productCreated);
   };
+
+  public getAll = async (_req:Request, res: Response) => {
+    const products = await this.productService.getAll();
+    res.status(statusCodes.OK).json(products);
+  };
 }
