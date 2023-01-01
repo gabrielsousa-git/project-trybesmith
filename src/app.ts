035dd45fae20751689d4662a4cafca5de0959ac0
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2023-01-01 10:48:30 -0300

    :sparkles: feat: Req 4 - Crie um endpoint para listar todos os pedidos

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import ProductRoutes from './routes/product.routes';
 import UserRoutes from './routes/user.routes';
+import OrderRoutes from './routes/order.routes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use(ProductRoutes);
 app.use(UserRoutes);
+app.use(OrderRoutes);
 
 export default app;
